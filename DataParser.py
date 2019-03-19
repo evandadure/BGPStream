a7ed4a6c02de28d77d6b1f99174c2815c79c7b23
Boulix <maxenceperso@gmail.com>
2019-03-19 17:35:39 +0100

    ajout des tables

--- DataParser.py
+++ DataParser.py
@@ -13,3 +13,30 @@ mycursor = mydb.cursor()
 sql = "INSERT INTO Tweet (created_at, text, user_ID, user_name, screen_name, latitude, longitude) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 val = ()
 mycursor.execute(sql, val)
+
+#DROP TABLE IF EXISTS Hijack;
+#
+#CREATE TABLE Hijack(
+# numAS varchar(255),
+# nomAS varchar(255),
+# paysAS varchar(255),
+# prefixe varchar(255),
+# adresseSource varchar(255),
+# adresseDesti varchar(255)
+#)
+#
+#CREATE TABLE Outage(
+# numAS varchar(255),
+# nomAS varchar(255),
+# paysAS varchar(255),
+# nbPrefixe varchar(255)
+#)
+#
+#CREATE TABLE Leak(
+# numAS varchar(255),
+# nomAS varchar(255),
+# paysAS varchar(255),
+# source varchar(255),
+# destination varchar(255)
+#)
+#)
