987b0957418a0e9c97f9a71e4580e45dec8bc43d
Evan Dadure <evan.dadure@gmail.com>
2019-03-26 16:30:21 +0100

    merge addToDB_HJ

--- DataParser.py
+++ DataParser.py
@@ -1,59 +1,97 @@
+import re
 import mysql.connector
 
 class dataParser():
     
     def __init__(self):
         self.mydb= mysql.connector.connect(
-                       host="tp-epu.univ-savoie.fr",
-                       port="3308",
-                       user="personma",
-                       passwd="rca8v7gd",
-                       database="personma"
+                       host="localhost",
+                       port="3306",
+                       user="root",
+                       passwd="root",
+                       database="bgpstreamdb"
                    )
-    
+
+    def find_nth(self, string, substring, n):
+       if (n == 1):
+           return string.find(substring)
+       else:
+           return string.find(substring, self.find_nth(string, substring, n - 1) + 1)
+
     def addToDB(self, tweet):
-        mycursor = mydb.cursor()
         text_tweet = tweet["text"].split(",")
         if text_tweet[1] == "OT":
-            return 
+            self.addToDB_OT(tweet)
         if text_tweet[1] == "HJ":
-            return
+            self.addToDB_HJ(tweet)
         if text_tweet[1] == "LK":
-            return
-        sql = "INSERT INTO Tweet (created_at, text, user_ID, user_name, screen_name, latitude, longitude) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        val = ()
-        mycursor.execute(sql, val)
-        
-        
-        
+            pass
+
+    def addToDB_OT(self, tweet):
+        mycursor = self.mydb.cursor()
+        text_tweet = tweet["text"].split(",")
+        text = text_tweet[2]
+        pattern = '\d+'
+        if re.match(pattern, text):
+            print(re.findall(pattern, text_tweet[6]))
+            nbPref = re.findall(pattern, text_tweet[6])[0]
+            val = (tweet["id_str"], text, text_tweet[3], text_tweet[4], nbPref)
+            sql = "INSERT INTO Outage (id, numAS, nomAS, paysAS, nbPrefixe) VALUES (%s, %s, %s, %s, %s)"
+            mycursor.execute(sql, val)
+        else:
+            nbPref = re.findall(pattern, text_tweet[5])[0]
+            val = (tweet["id_str"],  text_tweet[3], text, nbPref)
+            sql = "INSERT INTO Outage (id, numAS, nomAS, paysAS, nbPrefixe) VALUES (%s, 0, %s, %s, %s)"
+            mycursor.execute(sql, val)
+
+    def addToDB_HJ(self, tweet):
+        mycursor = self.mydb.cursor()
+        text_tweet = tweet["text"]
+        id_tweet = tweet["id_str"]
+        as_source = text_tweet[text_tweet.find("prefix")+9:self.find_nth(text_tweet," ",3)]
+        prefixe = text_tweet[self.find_nth(text_tweet," ",3)+1:self.find_nth(text_tweet,",",3)]
+        as_hijack = text_tweet[text_tweet.find(",-,By ")+8:text_tweet.find(" ",text_tweet.find(",-,By ")+8)]
+        print("id :",id_tweet,"prefixe :", prefixe, "numASSource :", as_source,"numASHijack :", as_hijack)
+        val = (id_tweet, prefixe, as_source, as_hijack)
+        sql = "INSERT INTO hijack (id, prefixe, numASSource, numASHijack) VALUES (%s, %s, %s, %s)"
+        # sql = "INSERT INTO hijack (id, prefixe, numASSource, numASHijack) VALUES ('"+id_tweet+"','"+prefixe+"','"+as_source+"','"+as_hijack+"')"
+        mycursor.execute(sql,val)
+
+        # mycursor = self.mydb.cursor()
+        # mycursor.execute("SELECT * FROM hijack")
+        # myresult = mycursor.fetchall()
+        # for line in myresult:
+        #     print(line)
+
+
 
 # =============================================================================
 #               SQL POUR CRÉER LES TABLES
-        
 # =============================================================================
 #DROP TABLE IF EXISTS Hijack;
-#
-#CREATE TABLE Hijack(
-# numAS varchar(255),
-# nomAS varchar(255),
-# paysAS varchar(255),
-# prefixe varchar(255),
-# adresseSource varchar(255),
-# adresseDesti varchar(255)
-#)
+#DROP TABLE IF EXISTS Outage;
+#DROP TABLE IF EXISTS Leak;
 #
 #CREATE TABLE Outage(
+# id varchar(255),
 # numAS varchar(255),
 # nomAS varchar(255),
 # paysAS varchar(255),
 # nbPrefixe varchar(255)
-#)
+#);
+#CREATE TABLE Hijack(
+# id varchar(255),
+# prefixe varchar(255),
+# numASSource varchar(255),
+# numASHijack varchar(255)
+#);
+#
 #
 #CREATE TABLE Leak(
+# id varchar(255),
 # numAS varchar(255),
 # nomAS varchar(255),
 # paysAS varchar(255),
 # source varchar(255),
 # destination varchar(255)
-#)
-#)
+#);
--- TwitterListener.py
+++ TwitterListener.py
@@ -1,5 +1,6 @@
-#Import the necessary methods from tweepy library
+# Import the necessary methods from tweepy library
 from tweepy.streaming import StreamListener
+from DataParser import dataParser
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy import API
@@ -9,11 +10,12 @@ import json
 with open('data/keys.json') as json_file:
     data = json.load(json_file)
 
+
 # =============================================================================
 # code inspiré de http://docs.tweepy.org/en/v3.4.0/streaming_how_to.html
 # =============================================================================
 
-#This is a basic listener that just prints received tweets to stdout.
+# This is a basic listener that just prints received tweets to stdout.
 class TwitterListener(StreamListener):
 
     def __init__(self):
@@ -26,26 +28,28 @@ class TwitterListener(StreamListener):
 
     def on_error(self, status):
         print(status)
-        
-        
-#   only the first 3000 tweets are available
+
+    #   only the first 3000 tweets are available
     def getPreviousTweets(self):
         api = API(auth)
-        tweets = []
-        for tweet in tweepy.Cursor(api.user_timeline,id='3237083798').items():
-            tweets.append(tweet._json)
-        return tweets[0]
+        #        tweets = []
+        for tweet in tweepy.Cursor(api.user_timeline, id='3237083798').items(10):
+            #            tweets.append(tweet._json)
+            data = dataParser()
+            data.addToDB(tweet._json)
+
+
+#        return tweets[0]
 
 
 if __name__ == '__main__':
-
-    #This handles Twitter authetification and the connection to Twitter Streaming API
+    # This handles Twitter authetification and the connection to Twitter Streaming API
     l = TwitterListener()
     auth = OAuthHandler(data['consumer_key'], data['consumer_secret'])
     auth.set_access_token(data['access_token'], data['access_token_secret'])
-#    stream = Stream(auth, l)
-#
-#    stream.filter(track=['poule'])
+    #    stream = Stream(auth, l)
+    #
+    #    stream.filter(track=['poule'])
 
-    print(l.getPreviousTweets())
-    #stream.filter(follow=['3237083798'])
+    l.getPreviousTweets()
+# stream.filter(follow=['3237083798'])
